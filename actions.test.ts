9452ee25b28bde44082c08d58307b68afa6f5358
stateblocks <mail634@protonmail.com>
2020-05-11 22:26:54 +0200

    Add helper function to enforce action map shape.

--- actions.test.ts
+++ actions.test.ts
@@ -1,4 +1,4 @@
-import {useActions} from "./actions";
+import {actionsOf, useActions} from "./actions";
 import {Counter, createMockCounterContext} from "./samples/Counter";
 
 
@@ -6,4 +6,13 @@ test("use actions", () => {
     useActions(Counter.actions).withContext({onReset(arg: number): void {
 
         }})
+})
+
+test("check actions shape", () => {
+    actionsOf<number, {a:() => void}>()({
+        increment:() => (state, exe) => {
+            return state + 1
+        }
+    })
+
 })
