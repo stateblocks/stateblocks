1e5b62663def32636ebfee05832e09bf0b845865
stateblocks <mail634@protonmail.com>
2020-06-02 01:43:10 +0200

    Add redux integration draft

--- src/stateblocksRedux.ts
+++ src/stateblocksRedux.ts
@@ -1,29 +1,72 @@
-import {Dispatch, Middleware} from "redux";
+import {Action, AnyAction, Dispatch, Middleware} from "redux";
 import {createStore as createSBStore} from "./store";
+import {ThunkAction} from "redux-thunk";
+import {Reducer} from "./core";
 
 
-export const defaultActionCreator = <S>(newState: S) => ({
+export function createTruc<T extends string>(actionType: string, argName: T) {
+    let reducer = <S>(state: S, action: { [P in T]: S }) => {
+        return action[argName]
+    }
+    return reducer
+}
+
+createTruc("test", "truc")(1, {truc: 1})
+
+
+export const stateblocksReducer = <S>(initialState:S, actionType = "STATEBLOCKS_UPDATE") => (state: S = initialState, action: DefaultStateUpdateAction<S>) => {
+    if (action.type == actionType) {
+        return action.newState
+    } else {
+        return state
+    }
+}
+
+export function applyReducer<S>(reducer:Reducer<S>, type = "STATEBLOCKS_ACTION"){
+    return {
+        type,
+        reducer
+    }
+}
+
+export interface DefaultStateUpdateAction<S> extends Action {
+    type: "STATEBLOCKS_UPDATE",
+    newState: S
+}
+
+
+export const defaultActionCreator = (actionType = "STATEBLOCKS_UPDATE") => <S>(newState: S) => ({
     type: "STATEBLOCKS_UPDATE",
     newState
 });
 
-export type ReduxActionCreator<S, A> = (newState: S) => A
+export type StateBlockDispatch<S> = {
+    (sbReducer: Reducer<S>): any;
+}
 
-export function useStateBlocks<S, A>(initialState: S, actionType: ReduxActionCreator<S, A>): Middleware {
-    return (store) => {
+export type StateUpdateActionCreator<S> = (newState: S) => any
 
-        const sbStore = createSBStore(0)
+export function useStateBlocks<S>(
+    stateUpdateActionsCreator: StateUpdateActionCreator<S> = defaultActionCreator(),
+    reduxActionMapper: (action: any) => Reducer<S> = reducer => reducer,
+    selector:(state:any) => any = state => state
+): Middleware<StateBlockDispatch<S>, S> {
+    return ({dispatch, getState}) => {
+
+        const sbStore = createSBStore(null)
 
         return (next: Dispatch) => {
 
-            sbStore.listener = (state: any) => next({
-                type: actionType,
-                newState: state
-            })
+            sbStore.listener = (state: S) => next(stateUpdateActionsCreator(state))
 
             return (action: any) => {
-                sbStore.state = store.getState() ?? initialState;
-                sbStore.update(action);
+                let reducer = reduxActionMapper(action)
+                if (reducer) {
+                    sbStore.state = selector(getState());
+                    sbStore.update(reducer);
+                } else {
+                    next(action as any)
+                }
             };
         };
     }
