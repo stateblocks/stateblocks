f9a08c3019b3441c33c74092cefbc7775cb6028c
stateblocks <mail634@protonmail.com>
2020-06-01 23:27:05 +0200

    Fix handleActionMap memoization. Memoization was too agressive : cancelling memoization for reducers would not work if the same action map was memoized twice.

--- src/memo.ts
+++ src/memo.ts
@@ -1,5 +1,9 @@
 import {memoize} from "lodash-es";
 
+/**
+ * Memoizer for functions with two arguments.
+ * @param fn
+ */
 export function memoize2(fn: (arg1: any, arg2: any) => any) {
     const curried = memoize((arg1: any) => {
         return memoize((arg2: any) => {
@@ -12,15 +16,60 @@ export function memoize2(fn: (arg1: any, arg2: any) => any) {
 
 }
 
-let metaMemo = memoize(memoize);
 
-export function memoizeN<F extends (...args: any[]) => any>(fn: (...args: any[]) => any): F {
-    let newVar2 = memoize((arg0: any) => {
-        return memoizeN((...args: any[]) => {
+/**
+ * Vararg memoizer.
+ * @param fn
+ */
+export function memoizeVarArgs<F extends (...args: any[]) => any>(fn: F): F {
+    let curryAndMemo = memoize((arg0: any) => {
+        return memoizeVarArgs((...rest: any[]) => {
+            return fn(arg0, ...rest)
+        })
+    });
+    let memoizedOneArg:F = null;
+    let memoized = (...args: Parameters<F>): ReturnType<F> => {
+        if (args.length == 0) {
+            if(memoizedOneArg == null){
+                memoizedOneArg = memoize(fn)
+            }
+            return memoizedOneArg()
+        }
+        if (args.length == 1) {
+            if(memoizedOneArg == null){
+                memoizedOneArg = memoize(fn)
+            }
+            return memoizedOneArg(args[0])
+        }
+        if (args.length == 2) {
+            return curryAndMemo(args[0])(args[1])
+        }
+
+        if (args.length > 2) {
+            let [first, ...rest] = args;
+            return curryAndMemo(first)(...rest)
+        }
+    };
+    return memoized as F
+}
+
+
+const metaMemo = memoize(memoize);
+
+/**
+ * Vararg memoizer.
+ * Warning : If called multiple times, the previously cached results of memoized function will be returned.
+ * This could lead to unexpected resulults.
+ * @param fn
+ * @deprecated
+ */
+export function metaMemoVarArgs<F extends (...args: any[]) => any>(fn: F): F {
+    let curryAndMemo = memoize((arg0: any) => {
+        return metaMemoVarArgs((...args: any[]) => {
             return fn(arg0, ...args)
         })
     });
-    let newVar = (...args: Parameters<F>): ReturnType<F> => {
+    let memoized = (...args: Parameters<F>): ReturnType<F> => {
         if (args.length == 0) {
             return metaMemo(fn)()
         }
@@ -28,13 +77,13 @@ export function memoizeN<F extends (...args: any[]) => any>(fn: (...args: any[])
             return metaMemo(fn)(args[0])
         }
         if (args.length == 2) {
-            return newVar2(args[0])(args[1])
+            return curryAndMemo(args[0])(args[1])
         }
 
         if (args.length > 2) {
             let [first, ...rest] = args;
-            return newVar2(first)(...rest)
+            return curryAndMemo(first)(...rest)
         }
     };
-    return newVar as F
+    return memoized as F
 }
